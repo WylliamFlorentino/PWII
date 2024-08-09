5ad8cf8a9f59f4e0821e1cc7a61ae2d6699dd921
wylliam <wylliamflorentino@gmail.com>
2024-08-08 22:22:26 -0300

    Primeiro commit para o github com o arquivo index

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Inicial do Site</title>
+</head>
+<body>
+    <h1>Página inicial do site</h1>    
+</body>
+</html>
