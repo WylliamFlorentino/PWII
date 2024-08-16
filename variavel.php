bc0d3b6d1153b545640c0d9ced2924b9a8f8a0de
wylliam <wylliamflorentino@gmail.com>
2024-08-15 22:30:38 -0300

    Aula de Variaves

--- variavel.php
+++ variavel.php
@@ -48,7 +48,30 @@
     </nav>
     
     <div class="container">
-        <h1>Página inicial do site</h1>
+        <h1>Variáveis em PHP</h1>
+        <p>$numero = 10;//variavel inteiro</p>
+        <p>$string = "10";</p>
+        <p>$string1 = "Wylliam Camisa 10";</p>
+        <p>$string2 = "Wylliam Camisa " .$numero;</p>
+        <p>$numero = "Wylliam Camisa $numero";</p>
+        <p>$valorDecimal = 10.50;</p>
+        <p>$Ativo = true;</p>
+        <p>/* Aqui é um bloco de comentario  */</p>
+        
+        <?php // tudo que esta dentro das  tag não parece na tela ?>
+
+        <?php // A não ser que usemos a função echo com string ?>
+        
+        <?php 
+            echo "Hello World !!"; 
+            echo "<br>";
+            $numero = 10;
+            $nome = "Wylliam";
+            $frase1 = "<p> $nome camisa $numero frase com paragrafo </p>";
+            $frase2 = $nome. " camisa " .$numero; // Concatenação Antiga
+            echo $frase1;
+            echo "$frase2 é o que tem dentro da variavel";
+        ?>
 
 
     </div>
